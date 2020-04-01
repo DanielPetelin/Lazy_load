58fea7def550266ed2836667723e036221ddec48
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-01 20:53:05 +0300

    add new

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ const options = {
     root: null,
     rootMargine: '0px',
     threshold: 0.1
-}
+};
 
 function handleImg(myImg, observer) {
-    
+    myImg.forEach(myImgSingle => {
+        console.log(myImgSingle.intersectionRatio);
+    });
 };
 
 const observer = new IntersectionObserver(handleImg, options);
